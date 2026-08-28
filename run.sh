497bdccf52bd5c0848d9b9cb342d113eefb6ffbf
Yann <elt.yann123@gmail.com>
2026-08-28 11:36:16 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,15 @@
+#!/bin/bash
 
+echo "=== Alchemia Auto Hunt ==="
+
+# Check ADB
+adb devices
+
+# Run
+python main.py
+
+# Show log kalau error
+if [ $? -ne 0 ]; then
+    echo "Error! Cek log:"
+    tail -30 data/logs/hunter.log
+fi
